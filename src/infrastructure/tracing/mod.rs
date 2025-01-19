1a4bc1d782202ff56083c966a6fbba8f82349b12
X3ne <X3ne@users.noreply.github.com>
2025-01-19 17:05:43 +0100

    fix(back): only display needed product data in the trace

--- src/infrastructure/tracing/mod.rs
+++ src/infrastructure/tracing/mod.rs
@@ -5,9 +5,7 @@ macro_rules! trace_peer_infos {
     ($peer_infos:expr) => {
         let span = tracing::span!(
             tracing::Level::INFO,
-            "request",
-            ip = %$peer_infos.ip_address.unwrap_or_else(|| "unknown".parse().unwrap()),
-            user_agent = ?$peer_infos.user_agent,
+            "product_infos",
             product = ?$peer_infos.product,
             version = ?$peer_infos.version,
             platform = ?$peer_infos.platform
