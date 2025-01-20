dba4c71fa6de83cbdaf718bfa1b3d475c9d5b467
Hayce <Haycee@users.noreply.github.com>
2025-01-21 00:14:34 +0100

    feat(app): add confirm password field in register form

--- app/src/components/register-form.tsx
+++ app/src/components/register-form.tsx
@@ -61,7 +61,8 @@ export function RegisterForm({
       username,
       first_name: firstName,
       last_name: lastName,
-      password
+      password,
+      confirm_password: password
     }
 
     register(registerData)
